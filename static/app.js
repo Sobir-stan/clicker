35e8d277dddeab43fdcad2f5cd257699ad577a6a
Asd <sobir0620@gmail.com>
2026-03-04 13:52:57 +0500

    Fix: Dynamic WSS protocol for Render HTTPS deployment

--- static/app.js
+++ static/app.js
@@ -1,17 +1,18 @@
 let ws;
-const wsUrl = `ws://${window.location.host}/ws/${window.username}`;
+const wsProtocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+const wsUrl = `${wsProtocol}//${window.location.host}/ws/${window.username}`;
 
 function connectWebSocket() {
     ws = new WebSocket(wsUrl);
-    
-    ws.onmessage = function(event) {
+
+    ws.onmessage = function (event) {
         const data = JSON.parse(event.data);
         if (data.type === 'state_update') {
             updateUI(data.state);
         }
     };
 
-    ws.onclose = function() {
+    ws.onclose = function () {
         setTimeout(connectWebSocket, 1000);
     };
 }
